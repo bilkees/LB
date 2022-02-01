493ced340877bb015c0407fd0a5e4300f12b3782
bilkees <bilkeeskv@gmail.com>
2022-02-01 11:05:34 +0400

    port changed to PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
-const port =process.env.port || 5000;
+const port =process.env.PORT || 5000;
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
