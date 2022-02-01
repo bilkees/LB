46a18efca1199bcd884aee80c0b54a48f8b08dd0
bilkees <bilkeeskv@gmail.com>
2022-02-01 16:33:24 +0400

    dbname  Librarydb added in url

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 const port =process.env.PORT || 5000;
-const url = 'mongodb+srv://bilkees:bilkees@cluster0.urlh5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const url = 'mongodb+srv://bilkees:bilkees@cluster0.urlh5.mongodb.net/Librarydb?retryWrites=true&w=majority';
 // MongoClient.connect(url,(err,client)=>{
 //   if (err) return console.error(err)
 //   console.log('Connected to Database')
@@ -37,16 +37,3 @@ app.listen(port,()=> {
 
   .catch(console.error)
 
-// app.use(bodyParser.urlencoded({ extended: true }))
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html')
-//   })
-
-//   app.post('/books', (req, res) => {
-//     console.log(req.body)
-//   })
-  
-
-// app.listen(port,()=> {
-//     console.log('listening on 5000')
-//   })
