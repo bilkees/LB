c03ea8501a5452403c39897619a1ead7e7b9115d
bilkees <bilkeeskv@gmail.com>
2022-02-01 12:23:30 +0400

    collection book added

--- server.js
+++ server.js
@@ -4,20 +4,49 @@ const MongoClient = require('mongodb').MongoClient
 const app = express()
 const port =process.env.PORT || 5000;
 const url = 'mongodb+srv://bilkees:bilkees@cluster0.urlh5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-MongoClient.connect(url,(err,client)=>{
-  if (err) return console.error(err)
-  console.log('Connected to Database')
-});
-app.use(bodyParser.urlencoded({ extended: true }))
+// MongoClient.connect(url,(err,client)=>{
+//   if (err) return console.error(err)
+//   console.log('Connected to Database')
+// });
+
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('Librarydb')
+    const bookCollection = db.collection('books')
+
+    app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
   })
 
   app.post('/books', (req, res) => {
-    console.log(req.body)
+    bookCollection.insertOne(req,body)
+    .then(result=> {
+      console.log(result)
+    })
+    // console.log(req.body)
+    .catch(error=>console.error(error))
   })
   
 
 app.listen(port,()=> {
     console.log('listening on 5000')
   })
+  })
+
+  .catch(console.error)
+
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html')
+//   })
+
+//   app.post('/books', (req, res) => {
+//     console.log(req.body)
+//   })
+  
+
+// app.listen(port,()=> {
+//     console.log('listening on 5000')
+//   })
