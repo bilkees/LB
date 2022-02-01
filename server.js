6b777ae3c715f8a152a580a4cf429a99f769441c
bilkees <bilkeeskv@gmail.com>
2022-02-01 11:59:02 +0400

    password added

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 const port =process.env.PORT || 5000;
-
-
+const url = 'mongodb+srv://bilkees:bilkees@cluster0.urlh5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+MongoClient.connect(url,(err,client)=>{
+  if (err) return console.error(err)
+  console.log('Connected to Database')
+});
 app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
